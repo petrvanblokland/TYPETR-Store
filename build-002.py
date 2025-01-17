e1b5183cf20769cb91e925a0f2531c097c0201f2
petrvanblokland <buro@petr.com>
2025-01-17 21:12:53 +0100

    Update build-002.py

--- build-002.py
+++ build-002.py
@@ -36,7 +36,7 @@ for year in range(2018, 2026):
 
 if os.path.exists('_docs/'):
     EXPORT_PATH = '_docs/' # Jasper computer
-    MAMP_PATH = '/Applications/MAMP/htdocs/typetr'
+    MAMP_PATH = '/Applications/MAMP/htdocs'
 else:
     EXPORT_PATH = 'docs/'
     MAMP_PATH = '/Users/petr/Sites/localhost/typetr' # Petr computer
