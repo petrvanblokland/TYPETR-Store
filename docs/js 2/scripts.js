2ea8c1ab4d6113416e164c56b07e1210322cded0
JaspervanBlokland <jaspervanblokland@hotmail.com>
2025-12-25 20:59:32 +0100

    Changed the hover color to magenta (#E31C79)

---
+++ docs/js 2/scripts.js
@@ -0,0 +1,47 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+
+    slides.forEach((slide, index) => {
+        slide.style.opacity = "0";
+        slide.style.zIndex = "0";
+        slide.classList.remove("active");
+    });
+
+    slideIndex = (slideIndex + 1) % slides.length;
+
+    const currentSlide = slides[slideIndex];
+    currentSlide.style.opacity = "1";
+    currentSlide.style.zIndex = "1";
+    currentSlide.classList.add("active");
+
+    setTimeout(showSlides, 3000);
+}
+
+function toggleMenu() {
+  const menu = document.getElementById("navbar");
+  menu.style.display = (menu.style.display === "none") ? "block" : "none";
+}
+
+//document.addEventListener('DOMContentLoaded', function() {
+//    showSlides();
+//});
+
+
+const popup = document.getElementById('popup');
+
+window.addEventListener('click', function(event) {
+if (event.target === popup) {
+  popup.style.display = 'none';
+}
+});
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('click', function(event) {
+if (event.target === navbar) {
+  navbar.style.display = 'none';
+}
+});
+
